265c8358f05922f9fbf7ff383424c0d81780abe1
Coleman Sandefur <coleman.sandefur@gmail.com>
2026-07-21 20:53:02 -0500

    Removed conditional adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,15 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import node from "@astrojs/node";
 import netlify from "@astrojs/netlify";
-
-const isProduction = process.env.NODE_ENV === "production";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://wafflenotes.netlify.app",
 
-  ...(isProduction
-    ? {
-        adapter: netlify(),
-      }
-    : {
-        adapter: node({
-          mode: "standalone",
-        }),
-      }),
+  adapter: netlify({
+    imageCDN: false,
+  }),
 
   integrations: [sitemap(), react(), markdoc(), keystatic()],
 
